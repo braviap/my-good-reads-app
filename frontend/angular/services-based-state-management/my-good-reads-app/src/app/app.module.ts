afa7305f79cb9f5fe443cc75d503b19d7601ecbc
sundarcodes <sundar82@gmail.com>
2017-11-16 12:05:50 +0530

    1. Angular app initialized 2. Prototype of Good reads home page done

---
+++ frontend/angular/services-based-state-management/my-good-reads-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ReadFormComponent } from './read-form/read-form.component';
+
+import { routes } from './app.routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ReadFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/angular/services-based-state-management/my-good-reads-app/src/app/read-form/read-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-read-form',
+  templateUrl: './read-form.component.html',
+  styleUrls: ['./read-form.component.css']
+})
+export class ReadFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
